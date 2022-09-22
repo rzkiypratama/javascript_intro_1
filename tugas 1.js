0ac3797d2d9aa977c5f2090c278a14ff1ccc3a3e
putra pratama <107741831+rzkiyprtm@users.noreply.github.com>
2022-09-23 00:49:06 +0800

    Add files via upload
    homework tugas 1

---
+++ tugas 1.js
@@ -0,0 +1,18 @@
+const bioSaya = {
+    name: "Rizky Putra Pratama",
+    age: 26,
+    hobbies: ["Playing Games", "Midnight Driving", "Quality time with my family"],
+    isMarried: true,
+    schoolList: [
+        {Name:'SD Negeri Banjarbaru 6', year_in:'2005', year_out:'2008'},
+        {Name:'SMP Negeri Banjarbaru 5', year_in:'2008', year_out:'2011'},
+        {Name:'SMK Negeri Banjarbaru 1', year_in:'2011', year_out:'2014', Major:'Desain Komunikasi Visual'}
+        ],
+    skills: [
+        {skill_name: "Fingerstyle Guitar", level:'Advance'},
+        {skill_name: "Corel Draw", level:"Advance"}
+    ],
+    interestingCoding: true,
+}
+
+// console.log("Hai, nama saya " + bioSaya.name + ", saya berumur " + bioSaya.age + " tahun. Pendidikan terakhir saya bersekolah di " + bioSaya.schoolList[2].Name + " jurusan " + bioSaya.schoolList[2].Major + " dan lulus di tahun " + bioSaya.schoolList[2].year_out)
